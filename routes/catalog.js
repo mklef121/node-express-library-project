af825e1def3df2e676f0fc6096ceaf102ea4ef61
= <mklef121@gmail.com>
2021-01-07 18:51:43 +0100

    read up and documented rules on mongo db database

--- routes/catalog.js
+++ routes/catalog.js
@@ -16,6 +16,8 @@ router.get('/', book_controller.index);
 // GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
 router.get('/book/create', book_controller.book_create_get);
 
+// GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
+router.post('/book/create', book_controller.book_create_post);
 
 // GET request to delete Book.
 router.get('/book/:id/delete', book_controller.book_delete_get);
